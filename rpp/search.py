58ef40f05708483f0171a88221fe68973a3b0f03
Vedang Waradpande <vedang@nbp-25-202-113.nbp.ruw.rutgers.edu>
2021-09-29 20:31:07 -0400

    Add Repeated A* search to search.py

--- rpp/search.py
+++ rpp/search.py
@@ -189,3 +189,209 @@ def move_and_record(start, goal, grid, planned_path, knowledge, nbhd_type="compa
         steps += 1
 
     return planned_path[i], knowledge, steps, last_open_cell
+
+
+class RepeatedAStar:
+
+    def __init__(self, grid, move_nbhd_type="compass", epsilon=1.,
+                 escape_tunnels=False):
+        """
+        Implements the Repeated A* algorithm on a grid-world.
+
+        Parameters
+        ----------
+        grid: 2D np.array
+          The grid world on which we need to implement the algorithm
+        move_nbhd_type: str
+          Can be 'compass' (see in all 4 directions in the execution phase)
+          or 'directional' (see in only the direction of the movement
+          in the execution phase)
+        epsilon: float
+          Weight with which the heuristic function is multiplied, as:
+          f(n) = g(n) + epsilon*h(n)
+        escape_tunnels: bool
+          Set to true to restart A* from the start of a tunnel by backtracking
+          instead of dead-ends
+        """
+
+        # Logging
+        self.knowledge_snaps = []
+        self.visited_snaps = []
+        self.start_end_snaps = []
+        self.sol_snaps = []
+        self.step = 0
+        self.successfully_completed = False
+        self.final_exit_status = None
+        self.grid = grid
+        self.total_cells_processed_by_run = []
+        self.backtracks = []
+
+        # Algorithm parameters
+        self.epsilon = epsilon
+        self.move_nbhd_type = move_nbhd_type
+        self.escape_tunnels = escape_tunnels
+
+    def _update_state(self, knowledge, visited, path_end_points, soln,
+                      step, successfully_completed, final_exit_status,
+                      num_cells_popped):
+        """
+        Updates the state (logging variables) after each run of the planning + execution phases.
+
+        Parameters
+        ----------
+        knowledge: 2D np.array
+          Represents the knowledge of the agent. knowledge[x][y] = 1 if
+          the agent knows there exists a block at position (x, y) and is 0 otherwise.
+        visited: 2D np.array
+          Represents which nodes the agent has already expanded. visited[x][y] = 1 if
+          the agent has already expanded Node at (x, y) and 0 otherwise.
+        path_end_points: ((int, int), (int, int))
+          Start and end points for 1 run of A* search
+        soln: List[(int, int)]
+          Planned path from the start node to the end node for 1 run of A* search
+        step: int
+          The step number for repeated A* search
+        successfully_completed: bool
+          If goal node has been reached by repeated A* search
+        final_exit_status: str
+          `"SUCCESS"` - If A* was successfully completed
+          `"RUNNING"` - If Repeated A* search is still running
+          `"FAILED_NOPATH"` - A* search could not find a path between the start and end nodes
+          `"FAILED_STEPS"` - No. of steps in 1 run of A* search exceeded max steps
+          `"FAILED_STEPS_REP"` - No. of steps for repeated A* search exceeded max steps
+        num_cells_popped: int
+          Number of cells popped during one run of A* search
+
+
+        TODO
+        ----
+        - Add backtracks to the final path
+        """
+
+        self.knowledge_snaps.append(knowledge)
+        self.visited_snaps.append(visited)
+        self.start_end_snaps.append(path_end_points)
+        self.sol_snaps.append(soln)
+        self.step = step
+        self.successfully_completed = successfully_completed
+        self.final_exit_status = final_exit_status
+        self.total_cells_processed_by_run.append(num_cells_popped)
+
+    def logs(self):
+        """
+        Fetches all the logging parameters
+
+        Returns
+        -------
+        log: Dict[str, Any]
+          All logging vars added to a dictionary
+        """
+        return {
+            "grid": self.grid,
+            "knowledge": self.knowledge_snaps,
+            "visited": self.visited_snaps,
+            "start_end": self.start_end_snaps,
+            "solns": self.sol_snaps,
+            "num_steps": self.step,
+            "successfully_completed": self.successfully_completed,
+            "final_exit_status": self.final_exit_status
+        }
+
+    def path_followed(self):
+        """ Returns the actual path followed (planning + execution) """
+
+        # If the paths followed and solutions don't match
+        assert len(self.sol_snaps) == len(self.start_end_snaps), \
+            "More steps for A* than elements in start_end."
+
+        full_path = []
+        for i in range(len(self.start_end_snaps)):
+            # Get index of end node in path
+            _, end_node = self.start_end_snaps[i]
+            end_index = self.sol_snaps[i].index(end_node)
+
+            # Clip the array according to end index and append to parent array
+            clipped_array = self.sol_snaps[i][:end_index]
+            full_path += clipped_array
+
+        return full_path
+
+    def total_cells_processed(self):
+        return sum(self.total_cells_processed_by_run)
+
+    def total_backtracked_cells(self):
+        flat_list = [item for sublist in self.backtracks for item in sublist]
+        return len(flat_list)
+
+    def search(self, start, goal, grid, heuristic_type,
+               max_steps_astar=None, max_steps_repeated=None):
+
+        # Exit condition
+        if max_steps_repeated is None:
+            max_steps_repeated = grid.shape[0] * grid.shape[1]
+
+        # Initialize visited matrix, knowledge matrix and planned_path array
+        knowledge = np.zeros(grid.shape)
+
+        # Start
+        while not self.successfully_completed:
+
+            # If number of steps have been exceeded for Repeated A*
+            if self.step > max_steps_repeated:
+                self._update_state(None, None, (None, None),
+                                   None, self.step, False,
+                                   "FAILED_STEPS_REP", 0)
+                return
+
+            visited = np.zeros(grid.shape)
+
+            # Run A* search algorithm once
+            planned_path, exit_status, num_cells_popped = \
+                a_star_search(start, goal, grid, heuristic_type,
+                              visited, knowledge, max_steps=max_steps_astar,
+                              epsilon=self.epsilon)
+
+            # If no path can be found
+            if exit_status == "FAILED_NOPATH":
+                self._update_state(knowledge, visited, (start, None),
+                                   planned_path, self.step + 1, False,
+                                   exit_status, num_cells_popped)
+                return
+
+            # If num of steps were exceeded for one run of A*
+            elif exit_status == "FAILED_STEPS":
+                self._update_state(knowledge, visited, (start, None),
+                                   planned_path, self.step + 1, False,
+                                   exit_status, num_cells_popped)
+                return
+
+            # Move robot along the grid on the planned path and record environment
+            final_node, knowledge, _, last_open_cell = move_and_record(start, goal, grid,
+                                                                       planned_path, knowledge,
+                                                                       nbhd_type=self.move_nbhd_type)
+
+            # If we were able to reach the goal successfully
+            if final_node == goal:
+                self._update_state(knowledge, visited, (start, final_node),
+                                   planned_path, self.step + 1, True,
+                                   "SUCCESS", num_cells_popped)
+                continue
+
+            # If we were not able to reach the goal successfully, repeat until possible
+            self._update_state(knowledge, visited, (start, final_node),
+                               planned_path, self.step + 1, False,
+                               "RUNNING", num_cells_popped)
+
+            # Backtracking to the start of tunnels if this parameter is set, else start from end of last path
+            if self.escape_tunnels and last_open_cell is not None and final_node != last_open_cell:
+                start = last_open_cell
+
+                loc_idx = planned_path.index(last_open_cell)
+                backtrack = reversed(planned_path[loc_idx:])
+
+                self.backtracks.append(backtrack)
+            else:
+                start = final_node
+
+        # We were able to reach successfully
+        return
